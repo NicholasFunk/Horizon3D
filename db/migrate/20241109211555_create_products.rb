30c9d067697a48a48e72037f695e448395e94e59
nfunk <nf4313@hotmail.com>
2024-11-09 15:21:50 -0600

    Added the proper attributes to the Product table.

--- db/migrate/20241109211555_create_products.rb
+++ db/migrate/20241109211555_create_products.rb
@@ -1,6 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[7.2]
   def change
     create_table :products do |t|
+      t.string :name
+      t.string :email
+      t.string :address
+      t.integer :phone
+
       t.timestamps
     end
   end
