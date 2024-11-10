bc107049dee11be0d87e6caeb4a96ee418c210dc
nfunk <nf4313@hotmail.com>
2024-11-09 22:37:38 -0600

    Added a condition for previous and next page buttons on the index page.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,16 @@
 class ProductsController < ApplicationController
-  PRODUCTS_PER_PAGE = 10
+  PRODUCTS_PER_PAGE = 20
+  NUMBER_OF_PRODUCTS = Product.count
+
 
   def index
+    @page_max = (NUMBER_OF_PRODUCTS / PRODUCTS_PER_PAGE) - 1
     @page = params.fetch(:page, 0).to_i
     @products = Product.offset(@page * PRODUCTS_PER_PAGE).limit(PRODUCTS_PER_PAGE)
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 
 end
