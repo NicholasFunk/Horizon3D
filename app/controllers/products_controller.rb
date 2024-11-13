4cb761ac74789d00d3cfea8d6ceb462ecc8f9139
nfunk <nf4313@hotmail.com>
2024-11-13 10:32:28 -0600

    Finalized working pagination.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,23 @@
 class ProductsController < ApplicationController
-  PRODUCTS_PER_PAGE = 18
-  NUMBER_OF_PRODUCTS = Product.count
+  PAGE_SIZE = 20
+  TOTAL_PRODUCTS = Product.count
 
 
   def index
     @page = params.fetch(:page, 0).to_i
-    @products = Product.offset(@page * PRODUCTS_PER_PAGE).limit(PRODUCTS_PER_PAGE)
+    @products = Product.offset(@page * PAGE_SIZE).limit(PAGE_SIZE)
+    @total_pages = (TOTAL_PRODUCTS + PAGE_SIZE - 1) / PAGE_SIZE
 
     if params[:search_by_category] && params[:search_by_category] != ""
         @products = @products.joins(:category).where(categories: {name: params[:search_by_category]})
+    end
+
+    if @page < 0
+      redirect_to products_path(page: 0)
+    end
+
+    if @page > @total_pages
+      redirect_to products_path(page: @total_pages)
     end
   end
 
