7c014c03377b94a2c5efcb12928b5c074db81820
nfunk <nf4313@hotmail.com>
2024-11-12 21:54:50 -0600

    Fixed Ransack attributes and associations for the admin page.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
     belongs_to :category
-    validates :name, :price, :description, presence: :true
+    validates :name, :price, :description, :category_id, presence: :true
+    
+    def self.ransackable_associations(auth_object = nil)
+        ["category"]
+    end
+    
+    def self.ransackable_attributes(auth_object = nil)
+        ["category_id", "created_at", "description", "id", "id_value", "name", "price", "stock_quantity", "updated_at"]
+    end
 end
