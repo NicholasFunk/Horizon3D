3f46adc0b6a2ac0afe40dfe5bc2182b61a5289b0
nfunk <nf4313@hotmail.com>
2024-11-12 20:17:20 -0600

    Added a category search bar on the index view of all products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,15 @@
 class ProductsController < ApplicationController
-  PRODUCTS_PER_PAGE = 24
+  PRODUCTS_PER_PAGE = 18
   NUMBER_OF_PRODUCTS = Product.count
 
 
   def index
-    @page_max = (NUMBER_OF_PRODUCTS / PRODUCTS_PER_PAGE) - 1
     @page = params.fetch(:page, 0).to_i
     @products = Product.offset(@page * PRODUCTS_PER_PAGE).limit(PRODUCTS_PER_PAGE)
+
+    if params[:search_by_category] && params[:search_by_category] != ""
+        @products = @products.joins(:category).where(categories: {name: params[:search_by_category]})
+    end
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,26 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+AdminUser.destroy_all
 Product.destroy_all
 Category.destroy_all
 
-# Generate some cateogories
-categories = ["Printer", "Filament", "Resin", "Safety"]
+
+# Generate some categories
+categories = ["Printer", "Material", "Safety", "Misc"]
 
 categories.each do |category|
     Category.create(name: category)
 end
 
-40.times do
-
+100.times do
+    category = Faker::Number.between(from: 1, to: 4)
     name = Faker::Commerce.product_name
     price = Faker::Commerce.price
     description = "This is a test product."
     stock_quantity = Faker::Number.number(digits: 2)
-    product = Product.create(name: name, price: price, description: description, stock_quantity: stock_quantity, category_id: 1)
+    product = Product.create(name: name, price: price, description: description, stock_quantity: stock_quantity, category_id: Category.pluck(:id).sample)
 
 end
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
