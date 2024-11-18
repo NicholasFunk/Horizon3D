b940f43eaf4138926ef3d31cfe07e1742be28a88
nfunk <nf4313@hotmail.com>
2024-11-17 19:37:25 -0600

    Added page number indicator at the bottom nav for pagination.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,7 @@
 class Contact < ApplicationRecord
     validates :phone, :email, presence: true
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["created_at", "email", "id", "phone", "updated_at"]
+    end
 end
