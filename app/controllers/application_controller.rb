d9bc37b3656619ab4810dcae61dcbf6ff5799b31
nfunk <nf4313@hotmail.com>
2024-11-19 10:55:36 -0600

    Added a helper method to set all categories. This allows the categories drop down to show all availabe categories.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,16 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
 
   before_action :initialize_session
+  before_action :set_categories
   helper_method :cart
 
   private
   def initialize_session
     session[:cart] ||= []
+  end
+
+  def set_categories
+    @categories = Category.all
   end
 
   def cart
