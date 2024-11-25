7650ee2efb909cc99315fbcbd87f387ecbf94dba
nfunk <nf4313@hotmail.com>
2024-11-25 16:28:44 -0600

    Added basic logic to the users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+    def show
+        # find the user in the database and display their information.
+        @user = User.find(params[:id])
+    end
+
+    def new
+        # creates a new user in the database
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            session[:user_id = @user.id]
+            redirect_to @user
+        else
+            flash[:error] = "Not a valid username or password"
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_17_224907) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_25_222313) do
   create_table "abouts", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
@@ -108,6 +108,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_17_224907) do
     t.datetime "updated_at", null: false
     t.integer "category_id", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "products", "categories"
