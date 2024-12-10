53357740ea3cae9acf753b064f7129e81f38463c
nfunk <nf4313@hotmail.com>
2024-12-10 11:05:48 -0600

    Added working image uploads to the products table in active storage.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Product do
-  permit_params :name, :description, :price, :stock_quantity, :category_id
+  permit_params :name, :description, :price, :stock_quantity, :category_id, :image
+  filter :image_attachment_id
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+    f.actions
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,13 @@
 class Product < ApplicationRecord
     belongs_to :category
     validates :name, :price, :description, :category_id, presence: :true
-
+    has_one_attached :image
     def self.ransackable_associations(auth_object = nil)
         [ "category" ]
     end
 
+
     def self.ransackable_attributes(auth_object = nil)
-        [ "category_id", "created_at", "description", "id", "id_value", "name", "price", "stock_quantity", "updated_at" ]
+        [ "category_id", "created_at", "description", "id", "id_value", "name", "price", "stock_quantity", "updated_at", "image" ]
     end
 end
